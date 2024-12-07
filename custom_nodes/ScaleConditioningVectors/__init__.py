c8ef405e14e7c02bfed772800af5c19d5e6f571c
Kristofer Schlachter <kristofe72@gmail.com>
2024-12-07 11:40:13 -0500

    Adding custom nodes I wrote

---
+++ custom_nodes/ScaleConditioningVectors/__init__.py
@@ -0,0 +1,5 @@
+# FILE: add_conditioning_vectors/__init__.py
+
+from .scale_conditioning_vectors import NODE_CLASS_MAPPINGS,NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS','NODE_DISPLAY_NAME_MAPPINGS']
---
+++ custom_nodes/SumConditioningVectors/add_conditioning_vectors.py
@@ -0,0 +1,160 @@
+class AddConditioningVectors:
+    """
+    A example node
+
+    Class methods
+    -------------
+    INPUT_TYPES (dict):
+        Tell the main program input parameters of nodes.
+    IS_CHANGED:
+        optional method to control when the node is re executed.
+
+    Attributes
+    ----------
+    RETURN_TYPES (`tuple`):
+        The type of each element in the output tuple.
+    RETURN_NAMES (`tuple`):
+        Optional: The name of each output in the output tuple.
+    FUNCTION (`str`):
+        The name of the entry-point method. For example, if `FUNCTION = "execute"` then it wilclass AddConditioningVectors:).execute()
+    OUTPUT_NODE ([`bool`]):
+        If this node is an output node that outputs a result/image from the graph. The SaveImage node is an example.
+        The backend iterates on these output nodes and tries to execute all their parents if their parent graph is properly connected.
+        Assumed to be False if not present.
+    CATEGORY (`str`):
+        The category the node should appear in the UI.
+    DEPRECATED (`bool`):
+        Indicates whether the node is deprecated. Deprecated nodes are hidden by default in the UI, but remain
+        functional in existing workflows that use them.
+    EXPERIMENTAL (`bool`):
+        Indicates whether the node is experimental. Experimental nodes are marked as such in the UI and may be subject to
+        significant changes or removal in future versions. Use with caution in production workflows.
+    execute(s) -> tuple || None:
+        The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
+        For example, if `FUNCTION = "execute"` then this method's name must be `execute`, if `FUNCTION = "foo"` then it must be `foo`.
+    """
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        """
+            Return a dictionary which contains config for all input fields.
+            Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
+            Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
+            The type can be a list for selection.
+
+            Returns: `dict`:
+                - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
+                - Value input_fields (`dict`): Contains input fields config:
+                    * Key field_name (`string`): Name of a entry-point method's argument
+                    * Value field_config (`tuple`):
+                        + First value is a string indicate the type of field or a list for selection.
+                        + Second value is a config for type "INT", "STRING" or "FLOAT".
+        """
+        return {
+            "required": {
+                "cond1": ("CONDITIONING",),
+                "cond2": ("CONDITIONING",),
+                "print_to_screen": (["enable", "disable"],),
+            },
+        }
+
+    RETURN_TYPES = ("CONDITIONING",)
+    #RETURN_NAMES = ("image_output_name",)
+
+    FUNCTION = "go"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "Custom"
+
+    def check_lazy_status(self,cond1, cond2, print_to_screen):
+        """
+            Return a list of input names that need to be evaluated.
+
+            This function will be called if there are any lazy inputs which have not yet been
+            evaluated. As long as you return at least one field which has not yet been evaluated
+            (and more exist), this function will be called again once the value of the requested
+            field is available.
+
+            Any evaluated inputs will be passed as arguments to this function. Any unevaluated
+            inputs will have the value None.
+
+        """
+        if print_to_screen == "enable":
+            return ["cond1", "cond2"]
+        else:
+            return []
+
+    def go(self, cond1, cond2, print_to_screen):
+        if print_to_screen == "enable":
+            '''
+            print(f"""Your input contains:
+                #cond1: {cond1}
+                #cond2: {cond2}
+                type(cond1): {type(cond1)}
+                type(cond2): {type(cond2)}
+                len(cond1): {len(cond1)}
+                len(cond1[0]): {len(cond1[0])}
+                cond1[0][0]: {cond1[0][0]}
+                cond1[0][1]: {cond1[0][1]}
+            """)
+            '''
+        #do some processing on the image, in this example I just invert it
+        def sum_conditioning_vectors(cond1, cond2):
+            result = []
+            sublist = []
+            result.append(sublist)
+            sublist.append(cond1[0][0] + cond2[0][0])
+            sublist.append({'pooled_output': cond1[0][1]['pooled_output'] + cond2[0][1]['pooled_output']})
+            return result
+        def multiply_conditioning_vectors(cond, multiplier):
+            result = []
+            sublist = []
+            result.append(sublist)
+            sublist.append(cond[0][0] * multiplier)
+            sublist.append({'pooled_output': cond[0][1]['pooled_output'] * multiplier})
+            return result
+        s = sum_conditioning_vectors(cond1, cond2)
+        #avg = multiply_conditioning_vectors(avg, 0.5)
+
+        return (s,)
+
+    """
+        The node will always be re executed if any of the inputs change but
+        this method can be used to force the node to execute again even when the inputs don't change.
+        You can make this node return a number or a string. This value will be compared to the one returned the last time the node was
+        executed, if it is different the node will be executed again.
+        This method is used in the core repo for the LoadImage node where they return the image hash as a string, if the image hash
+        changes between executions the LoadImage node is executed again.
+    """
+    #@classmethod
+    #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
+    #    return ""
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "AddConditioningVectors": AddConditioningVectors
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AddConditioningVectors": "AddConditioningVectors Node"
+}
+
+
+# Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
+# WEB_DIRECTORY = "./somejs"
+
+
+'''
+# Add custom API routes, using router
+from aiohttp import web
+from server import PromptServer
+
+@PromptServer.instance.routes.get("/hello")
+async def get_hello(request):
+    return web.json_response("hello")
+'''
