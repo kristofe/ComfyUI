7201aa9800b87e6c7a6f23ffe95fabb83a33a6f5
Kristofer Schlachter <kristofe72@gmail.com>
2024-12-10 18:25:08 -0500

    Started fisheye projection code

---
+++ custom_nodes/FisheyProjection/__init__.py
@@ -0,0 +1,5 @@
+# FILE: add_conditioning_vectors/__init__.py
+
+from .fisheye_projection import NODE_CLASS_MAPPINGS,NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS','NODE_DISPLAY_NAME_MAPPINGS']
